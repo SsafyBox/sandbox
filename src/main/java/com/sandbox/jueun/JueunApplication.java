83a2ba2b36143df252aaa287024a2024d027414b
이주은 <wndms058@gmail.com>
2024-10-20 16:40:14 +0900

    init

---
+++ src/main/java/com/sandbox/jueun/JueunApplication.java
@@ -0,0 +1,13 @@
+package com.sandbox.jueun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JueunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JueunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandbox/jueun/JueunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.jueun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JueunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
