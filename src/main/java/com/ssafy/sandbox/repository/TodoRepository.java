d922c92e3ab7c8912e616865d7fb32000fec1f6c
최수현 <sw92522@naver.com>
2024-10-21 23:06:06 +0900

    feat: update todo

--- src/main/java/com/ssafy/sandbox/repository/TodoRepository.java
+++ src/main/java/com/ssafy/sandbox/repository/TodoRepository.java
@@ -24,4 +24,13 @@ public class TodoRepository {
         return em.createQuery("select t from Todo t", Todo.class)
                 .getResultList();
     }
+
+    public Todo findOne(Long id) {
+        return em.find(Todo.class, id);
+    }
+
+    public Long update(Todo todo) {
+        em.merge(todo);
+        return todo.getId();
+    }
 }
