3b334da6ec4d63d8604965a57d8fbf6be4226331
최수현 <sw92522@naver.com>
2024-10-21 22:32:18 +0900

    feat: select todo list

--- src/main/java/com/ssafy/sandbox/controller/TodoController.java
+++ src/main/java/com/ssafy/sandbox/controller/TodoController.java
@@ -4,9 +4,12 @@ import com.ssafy.sandbox.domain.Todo;
 import com.ssafy.sandbox.service.TodoService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +23,11 @@ public class TodoController {
         return new CreateTodoResponse(id);
     }
 
+    @GetMapping("/todos")
+    public List<Todo> findAllTodo() {
+        return todoService.findAll();
+    }
+
     @Data
     static class CreateTodoResponse {
         private Long id;
@@ -28,4 +36,15 @@ public class TodoController {
             this.id = id;
         }
     }
+
+//    @Data
+//    static class FindTodoResponse {
+//        private Long id;
+//        private String content;
+//
+//        public FindTodoResponse(Long id, String content) {
+//            this.id = id;
+//            this.content = content;
+//        }
+//    }
 }
