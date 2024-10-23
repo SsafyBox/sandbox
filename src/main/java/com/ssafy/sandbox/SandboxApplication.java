5d9da8e3a18dea90342081e90d4cd8fddd79db32
최수현 <sw92522@naver.com>
2024-10-23 13:57:25 +0900

    fix: hide MySQL config

--- src/main/java/com/ssafy/sandbox/SandboxApplication.java
+++ src/main/java/com/ssafy/sandbox/SandboxApplication.java
@@ -2,7 +2,9 @@ package com.ssafy.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:secret.properties")
 @SpringBootApplication
 public class SandboxApplication {
 
