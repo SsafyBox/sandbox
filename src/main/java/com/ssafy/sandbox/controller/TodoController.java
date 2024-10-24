23e59e2c83d855a4a505aa1f5b835d4aa319dd67
최수현 <sw92522@naver.com>
2024-10-24 21:50:58 +0900

    refactor:  ResponseEntity를 사용하여 응답 반환

--- src/main/java/com/ssafy/sandbox/controller/TodoController.java
+++ src/main/java/com/ssafy/sandbox/controller/TodoController.java
@@ -3,10 +3,12 @@ package com.ssafy.sandbox.controller;
 import com.ssafy.sandbox.domain.Todo;
 import com.ssafy.sandbox.dto.request.CreateTodoRequest;
 import com.ssafy.sandbox.dto.response.FindAllTodoResponse;
+import com.ssafy.sandbox.dto.response.MessageResponse;
 import com.ssafy.sandbox.dto.response.TodoIdResponse;
 import com.ssafy.sandbox.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,32 +17,31 @@ public class TodoController {
 
     private final TodoService todoService;
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/todos")
-    public TodoIdResponse saveTodo(@RequestBody CreateTodoRequest todoDto) {
+    public ResponseEntity<TodoIdResponse> saveTodo(@RequestBody CreateTodoRequest todoDto) {
         Todo todo = new Todo();
         todo.setContent(todoDto.getContent());
 
-        return todoService.save(todo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(todoService.save(todo));
     }
 
     @GetMapping("/todos")
-    public FindAllTodoResponse findAllTodo() {
+    public ResponseEntity<FindAllTodoResponse> findAllTodo() {
 
-        return todoService.findAll();
+        return ResponseEntity.ok(todoService.findAll());
     }
 
     @PatchMapping("/todos/{id}")
-    public TodoIdResponse updateTodo(@PathVariable Long id) {
+    public ResponseEntity<TodoIdResponse> updateTodo(@PathVariable Long id) {
 
-        return todoService.update(id);
+        return ResponseEntity.ok(todoService.update(id));
     }
 
     @DeleteMapping("/todos/{id}")
-    public String deleteTodo(@PathVariable Long id) {
+    public ResponseEntity<MessageResponse> deleteTodo(@PathVariable Long id) {
         todoService.delete(id);
 
-        return "Deleted Successfully";
+        return ResponseEntity.ok(new MessageResponse("Deleted Successfully"));
     }
 
 }
