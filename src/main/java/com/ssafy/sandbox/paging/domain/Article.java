db223266a45f38c4e79099c697faa85864638f0e
suhyun <sw92522@naver.com>
2024-10-27 22:52:03 +0900

    feat: Entity 생성

--- src/main/java/com/ssafy/sandbox/paging/domain/Article.java
+++ src/main/java/com/ssafy/sandbox/paging/domain/Article.java
@@ -1,7 +1,23 @@
 package com.ssafy.sandbox.paging.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
